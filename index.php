3f76b26840821079a6976e858c0e89bb62b1414d
MKkevin2712 <kevinnkamen@gmail.com>
2025-11-17 00:23:19 +0100

    Correction de l'inclusion des fichiers avec __DIR__

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 
 // Réponse immédiate pour les requêtes OPTIONS (pré-vol CORS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
+  http_response_code(200);
+   exit;
 }
 
 
@@ -43,83 +43,83 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 // MODULE 1 : AUTHENTIFICATION
 if ($path === '/auth/register' && $method === 'POST') {
-    handleRegister($data);
+   handleRegister($data);
 } elseif ($path === '/auth/login' && $method === 'POST') {
-    handleLogin($data);
+    handleLogin($data);
 } elseif ($path === '/auth/logout' && $method === 'POST') {
-    handleLogout();
+    handleLogout();
 }
 
 // MODULE 4 : DASHBOARD
 elseif ($path === '/dashboard/stats' && $method === 'GET') {
-    handleGetStats();
+    handleGetStats();
 } elseif ($path === '/dashboard/urgent' && $method === 'GET') {
-    handleGetUrgentTasks();
+    handleGetUrgentTasks();
 }
 
 // MODULE 5 : ADMINISTRATION
 elseif ($path === '/admin/users' && $method === 'GET') {
-    handleListAllUsers();
+    handleListAllUsers();
 }
 // Routes ciblées sur un utilisateur Admin
 elseif (preg_match('/^\/admin\/users\/([a-f0-9-]+)$/i', $path, $matches)) {
-    $user_id = $matches[1];
-    if ($method === 'PUT') {
-        handleUpdateUserRole($user_id, $data);
-    } elseif ($method === 'DELETE') {
-        handleDeleteUser($user_id);
-    }
+    $user_id = $matches[1];
+    if ($method === 'PUT') {
+        handleUpdateUserRole($user_id, $data);
+    } elseif ($method === 'DELETE') {
+        handleDeleteUser($user_id);
+    }
 }
 
 // MODULE 3 : PARTAGE ET COMMENTAIRES
 // POST /tasks/{id}/share (Ajout collaborateur)
 elseif (preg_match('/^\/tasks\/([a-f0-9-]+)\/share$/i', $path, $matches)) {
-    $task_id = $matches[1];
-    if ($method === 'POST') {
-        handleShareTask($task_id, $data);
-    }
+    $task_id = $matches[1];
+   if ($method === 'POST') {
+        handleShareTask($task_id, $data);
+    }
 }
 // DELETE /tasks/{id}/share/{user_id} (Retrait collaborateur)
 elseif (preg_match('/^\/tasks\/([a-f0-9-]+)\/share\/([a-f0-9-]+)$/i', $path, $matches)) {
-    $task_id = $matches[1];
-    $user_id_to_remove = $matches[2];
-    if ($method === 'DELETE') {
-        handleUnshareTask($task_id, $user_id_to_remove);
-    }
+    $task_id = $matches[1];
+    $user_id_to_remove = $matches[2];
+    if ($method === 'DELETE') {
+       handleUnshareTask($task_id, $user_id_to_remove);
+    }
 }
 // GET/POST /tasks/{id}/comments
 elseif (preg_match('/^\/tasks\/([a-f0-9-]+)\/comments$/i', $path, $matches)) {
-    $task_id = $matches[1];
-    if ($method === 'POST') {
-        handleCreateComment($task_id, $data);
-    } elseif ($method === 'GET') {
-        handleReadComments($task_id);
-    }
+    $task_id = $matches[1];
+    if ($method === 'POST') {
+        handleCreateComment($task_id, $data);
+    } elseif ($method === 'GET') {
+        handleReadComments($task_id);
+        }
 }
 
 // MODULE 2 : CRUD TÂCHES (GÉNÉRAL ET SPÉCIFIQUE)
 // Routes ciblées sur une tâche spécifique
 elseif (preg_match('/^\/tasks\/([a-f0-9-]+)$/i', $path, $matches)) {
-    $task_id = $matches[1];
-    if ($method === 'GET') {
-        handleReadTaskDetail($task_id);
-    } elseif ($method === 'PUT') {
-        handleUpdateTask($task_id, $data);
-    } elseif ($method === 'DELETE') {
-        handleDeleteTask($task_id);
-    }
+    $task_id = $matches[1];
+    if ($method === 'GET') {
+        handleReadTaskDetail($task_id);
+    } elseif ($method === 'PUT') {
+      handleUpdateTask($task_id, $data);
+   } elseif ($method === 'DELETE') {
+     handleDeleteTask($task_id);
+   }
 }
 // Routes générales des tâches
 elseif ($path === '/tasks') {
-    if ($method === 'GET') {
-        handleReadTasks();
-    } elseif ($method === 'POST') {
-        handleCreateTask($data);
-    }
+    if ($method === 'GET') {
+       handleReadTasks();
+    } elseif ($method === 'POST') {
+        handleCreateTask($data);
+    }
 }
 
 
 // ROUTE NON TROUVÉE
 else {
-    json_response(['success' => false, 'message' => 'Endpoint non trouvé.'], 404);
+    json_response(['success' => false, 'message' => 'Endpoint non trouvé.'], 404);
 }
